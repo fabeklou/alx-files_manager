d190a23a68bb6845de5d2e89a1c66cbbed9844d2
Fabrice Eklou <fab.eklou@gmail.com>
2024-08-03 13:01:28 +0000

    feat: authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(400).send({ error: 'Missing authorization header !' });
+
+    const encodedCredentials = authorization.split(' ')[1];
+
+    try {
+      const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString('utf8');
+      const [email, password] = decodedCredentials.split(':');
+
+      if (!email || !password) return res.status(400).send({ error: 'Invalid credentials format !' });
+
+      const usersCollection = dbClient._db.collection('users');
+      const user = await usersCollection.findOne({ email, password: sha1(password) });
+
+      if (!user) return res.status(401).send({ error: 'Unauthorized ' });
+
+      const sessionToken = uuidv4();
+
+      /** 24 Hours -> 86400 Seconds */
+      await redisClient.set(`auth_${sessionToken}`, user._id.toString(), 86400);
+      return res.status(200).send({ token: sessionToken });
+    } catch (error) {
+      return res.status(400).send({ error: 'Invalid credentials format !' });
+    }
+  }
+
+  static async getDisconnect(req, res) {
+    const sessionToken = req.headers['x-token'];
+
+    if (!sessionToken) return res.status(400).send({ error: 'X-Token header is missing !' });
+
+    const userId = await redisClient.get(`auth_${sessionToken}`);
+
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const usersCollection = dbClient._db.collection('users');
+    const user = usersCollection.findOne({ _id: userId });
+
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    await redisClient.del(`auth_${sessionToken}`);
+    return res.status(204).end();
+  }
+}
+
+module.exports = AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -21,6 +23,23 @@ class UsersController {
     const enryptedPassword = sha1(password);
     const { insertedId } = await usersCollection.insertOne({ email, password: enryptedPassword });
     return res.status(201).json({ email, id: insertedId });
+  }
+
+  static async getMe(req, res) {
+    const sessionToken = req.headers['x-token'];
+
+    if (!sessionToken) return res.status(400).send({ error: 'X-Token header is missing !' });
+
+    const userId = await redisClient.get(`auth_${sessionToken}`);
+
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const usersCollection = dbClient._db.collection('users');
+    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    return res.send({ email: user.email, id: user._id.toString() });
   }
 }
 
